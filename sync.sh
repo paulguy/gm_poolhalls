b50643e1040af5fcc25b91d09303c23fdb0a3dea
Paul Guy <paulguy119@gmail.com>
2026-04-03 22:56:27 -0400

    Add music to areas in the map.
    Also fix sync script and music file locations.

--- sync.sh
+++ sync.sh
@@ -5,6 +5,6 @@ SYNCDIRS="maps materials models sound"
 
 for DIR in $SYNCDIRS; do
     echo "Syncing ${DIR}"
-    rsync -av --delete "$DIR" "${HOME}/.local/share/Steam/steamapps/common/GarrysMod/garrysmod/addons/$ADDONNAME/$DIR"
+    rsync -av --delete "$DIR" "${HOME}/.local/share/Steam/steamapps/common/GarrysMod/garrysmod/addons/$ADDONNAME"
 done
 
